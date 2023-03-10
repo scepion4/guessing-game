bd99820cf0237e9d2203e72b0367a9321278bb29
scepion4 <safonov04@ukr.net>
2023-03-10 08:50:24 +0200

    init:initial project

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor() {
+       
     }
 
-    guess() {
+    setRange(min, max,number) {
+    let middle=Math.floor(min+(max-min)/2)
+    if(middle==number){
+        guess()  
+    }
+    if(middle<number){
+        lower()
+    }
+    if(middle<number){
+        greater()
+    }
+    }
 
+    guess(middle) {
+      return middle 
     }
 
     lower() {
-
+        setRange(middle, max)  
     }
-
-    greater() {
-
+    greater(middle,setRange,min) {
+        setRange(min, middle)
     }
 }
 
